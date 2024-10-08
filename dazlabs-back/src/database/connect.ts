6b6b754cf940527e061efa392538c331a9bfcf27
Pedro Fernandez <pefernandez1408@gmail.com>
2024-10-08 19:00:02 -0400

    ADD: sincronizacion de la api con la base de datos

--- dazlabs-back/src/database/connect.ts
+++ dazlabs-back/src/database/connect.ts
@@ -1,9 +1,36 @@
 import { connect } from "mongoose";
+import axios from "axios";
+import CatModel from "../models/user.models.ts";
 
 export async function connection(): Promise<void> {
     const dbUri = process.env.DB_URI || ''
+    const urlCats = process.env.API_CATS_URL || ''
+    const apiKey = process.env.X_RAPIDAPI_KEY
     try {
         await connect(dbUri)
+        async function syncData(){
+            try {
+                const cats = await axios.get(urlCats, {
+                    headers: {
+                        "x-rapidapi-key": `${apiKey}`
+                    }
+                })
+                const catData = cats.data
+                catData.map(async (cat:any) => {
+                    return await CatModel.create({
+                        breed: cat.breed,
+                        origin: cat.origin,
+                        image: cat.img
+                    })
+                })
+                console.log('Datos sincronizados')
+            } catch (error) {
+                console.error('Error al sincronizar datos:', error);
+            }
+        }
+        //setInterval(syncData, 24 * 60 * 60 * 1000);
+        syncData()
+
         console.log('Database connected')
     } catch (error) {
         console.error(error)
