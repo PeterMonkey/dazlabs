60810c0c067a3bac0759a07f20398e6dcf908d6b
Pedro Fernandez <pefernandez1408@gmail.com>
2024-10-07 23:09:12 -0400

    ADD: manejo de excepciones

--- dazlabs-back/src/services/cats.services.ts
+++ dazlabs-back/src/services/cats.services.ts
@@ -1,51 +1,80 @@
-import { Response, Request } from "express";
+import { Response, Request, Handler } from "express";
 import CatModel, { ICats } from "../models/user.models.js";
 
 export class CatServices {
         
     async createCat(req: Request, res: Response): Promise<Response>{
-        const {breed, origin, image} = req.body;
-
-        const newCat = {
-            breed,
-            origin,
-            image,
+        try {
+            const {breed, origin, image} = req.body;
+    
+            const newCat = {
+                breed,
+                origin,
+                image,
+            }
+    
+            await CatModel.create(newCat)
+            return res.status(200).json({
+                ok: true,
+                response: newCat
+            })
+            
+        } catch (error) {
+            throw new Error()
         }
-
-        await CatModel.create(newCat)
-        return res.json({
-            ok: true,
-            response: newCat
-        })
     }
 
     async getCats(req: Request, res: Response): Promise<Response<ICats>>{
-        const cats = await CatModel.find()
-        return res.json({
-            cats
-        })
+        try {
+            const cats = await CatModel.find()
+            return res.json({
+                cats
+            })
+        } catch (error) {
+            throw new Error()
+        }
     }
 
     async updateCat(req: Request, res: Response): Promise<Response>{
-        const id = req.params.id;
-
-        const updateData = req.body;
-
-        await CatModel.updateOne({_id: id}, updateData)
-
-        return res.json({
-            ok: true,
-            message: `Cat with id ${id} updated`
-        })
+        try {
+            const id = req.params.id;
+    
+            const updateData = req.body;
+    
+            const cat = await CatModel.findByIdAndUpdate({_id: id}, updateData)
+            if(cat === null){
+                return res.status(404).json({
+                    ok: false,
+                    message: "Elemento no encontrado"
+                })
+            }
+            return res.status(200).json({
+                ok: true,
+                message: `Cat with id ${id} updated`
+            }) 
+    
+        } catch (error) {
+            throw new Error()
+        }
     }
 
-    async deleteCat(req: Request, res: Response){
-        const id = req.params.id;
-        await CatModel.deleteOne({_id: id})
-
-        return res.json({
-            ok: true,
-            message: `Cat with id ${id} deleted`
-        })
+    async deleteCat(req: Request, res: Response): Promise<Response>{
+        try {
+            const id = req.params.id;
+            const cat = await CatModel.findByIdAndDelete({_id: id})
+            if(cat === null) {
+                return res.status(404).json({
+                    ok: false,
+                    message: "Elemento no encontrado"
+                })
+            }
+            return res.json({
+                ok: true,
+                message: `Cat with id ${id} deleted`
+            })
+    
+        } catch (error) {
+            throw new Error()
+        }
     }
 }
